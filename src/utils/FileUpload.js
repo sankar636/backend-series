6341e400eb537f4f1d892e50e9cb1ab30f677527
sankarshan <ssankarshan636@gmail.com>
2025-04-08 10:23:09 +0530

    add multer middleware for file uploads and integrate Cloudinary for cloud storage

---
+++ src/utils/FileUpload.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+// upload image,video from local storage to cloudnary
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath){
+            return null;
+        }
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"})
+        // file has been uploaded successifully
+        console.log("File uploaded successifully on cloudinary", response.url);        
+        return response
+    } catch (error) {
+        console.log("Error in uploading the file", error);  
+        fs.unlinkSync(localFilePath) // this removes the locally saved file as the upload operation failed for this temporary file
+        return null      
+    }
+}
+
+export {uploadOnCloudinary}
+
+
+// Upload an image(Written in cloudinary)
+//  const uploadResult = await cloudinary.uploader
+//  .upload(
+//      'https://res.cloudinary.com/cloud_name/image/upload/getting-started/shoes.jpg', {
+//          public_id: 'shoes',
+//      }
+//  )
+//  .catch((error) => {
+//      console.log(error);
+//  });
+
+// console.log(uploadResult);
